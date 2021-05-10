d6ee074700747293ca53b7669d5bb642a26a2b82
Cheryl Hu <cheryl.hu@kellogg.northwestern.edu>
2021-05-10 01:28:47 -0500

    complete

--- netlify/functions/todos.js
+++ netlify/functions/todos.js
@@ -2,16 +2,16 @@
 exports.handler = async function(event) {
 
   // an Array of to-dos
-
+  let todos = [`buy milk`,`walk the dog`]
   // write the Array of to-dos to the *back-end* console
-
+  console.log(todos)
   // turn the Array into a String
-
+  let dataAsString = json.stringify(todos)
   // write the "stringified" Array to the *back-end* console
-
+  console.log(dataAsString)
   // a lambda function returns a status code and a string of data
   return {
     statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-    body: `Hello from the back-end!` // a string of data
+    body: dataAsString // `Hello from the back-end!` // a string of data
   }
 }
